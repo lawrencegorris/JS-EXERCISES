74106dd27b8e83459ebf1246ee026fa1b1b5206c
Lawrence Gorris <gorrislawrence.personal@gmail.com>
2021-05-03 16:53:04 +0200

    Added Objects exercises and solved 07-01

---
+++ 07-objects/01-generate-object/script.js
@@ -0,0 +1,28 @@
+/* becode/javascript
+ *
+ * /06-objects/01-generate-object/script.js - 6.1: générer un object
+ *
+ * coded by leny@BeCode
+ * started at 08/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    // your code here
+    const run = document.querySelector('#run');
+    const me = {
+        lastname: 'Gorris',
+        firstname: 'Lawrence',
+        age: 22,
+        city: 'Antwerp', 
+        country: 'Belgium'
+    }
+
+    run.addEventListener('click', showObject);
+    function showObject(){
+        console.log(me)
+    }
+
+})();
