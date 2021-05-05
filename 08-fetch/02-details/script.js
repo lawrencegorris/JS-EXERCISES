bbf2422a2afc6262dd7dbf732ec24406ec3cf7ed
Lawrence Gorris <gorrislawrence.personal@gmail.com>
2021-05-05 14:35:37 +0200

    Almost solved exercise 08-02 => fix content only showing on refresh

--- 08-fetch/02-details/script.js
+++ 08-fetch/02-details/script.js
@@ -11,14 +11,34 @@
 
 (() => {
     // your code here;
-    const id = parseInt(document.querySelector('#hero-id').value);
+
+    // creating const
+    const id = document.querySelector('#hero-id').value;
     const target = document.querySelector('#target');
     const run = document.querySelector('#run');
+    const template = document.querySelector('#tpl-hero');
+
+    async function getData(){
+        let data = await fetch(`http://localhost:3000/heroes?id=${id}`); // fetch data
+        let heroes = await data.json(); //turn data into JSON
+        return heroes; // return data in JSON format
+    }
+    // Event listener
     run.addEventListener('click', runFunction);
 
-   async function runFunction(){
-        const hero = await fetch('http://localhost:8000/_shared/api.json');
-        const showHero = hero.json();
-        console.log(showHero)
+    function runFunction(){
+        let specs = getData();
+        specs.then(function (result){
+            result = result [0];
+            console.log(specs);
+            console.log(result);
+
+            let tempCopy = template.content.cloneNode(true); // clones/copies html structure
+            tempCopy.querySelector('.name').innerHTML = result.name;
+            tempCopy.querySelector('.alter-ego').innerHTML = result.alterEgo;
+            tempCopy.querySelector('.powers').innerHTML = result.abilities;
+            target.appendChild(tempCopy);
+        })
     }
+
 })();
