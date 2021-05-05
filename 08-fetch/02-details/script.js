ddbabfca516fcc50017470ac71c306adbee05c87
Lawrence Gorris <gorrislawrence.personal@gmail.com>
2021-05-05 14:56:56 +0200

    Solved exercise; now trying to research and understand everything

--- 08-fetch/02-details/script.js
+++ 08-fetch/02-details/script.js
@@ -13,26 +13,23 @@
     // your code here;
 
     // creating const
-    const id = document.querySelector('#hero-id').value;
     const target = document.querySelector('#target');
     const run = document.querySelector('#run');
     const template = document.querySelector('#tpl-hero');
 
     async function getData(){
+        const id = document.querySelector('#hero-id').value;
         let data = await fetch(`http://localhost:3000/heroes?id=${id}`); // fetch data
         let heroes = await data.json(); //turn data into JSON
         return heroes; // return data in JSON format
     }
     // Event listener
-    run.addEventListener('click', runFunction);
+    run.addEventListener('click', runClicked);
 
-    function runFunction(){
+    function runClicked(){
         let specs = getData();
         specs.then(function (result){
             result = result [0];
-            console.log(specs);
-            console.log(result);
-
             let tempCopy = template.content.cloneNode(true); // clones/copies html structure
             tempCopy.querySelector('.name').innerHTML = result.name;
             tempCopy.querySelector('.alter-ego').innerHTML = result.alterEgo;
